d1efffef2757092d245ee01e5f97f4cee193688d
Jonathan Pillajo <pandc19@outlook.com>
2025-04-27 17:42:34 -0500

    Remove Front end URl from cors

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ dbConnection();
 var corsOptions = {
     origin: [
         'http://localhost:4200',
-        'https://pesaje.netlify.app',
     ],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
